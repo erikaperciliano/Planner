58341470659e85667ebf189c6616981b8e6069c5
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-07-09 14:02:50 +0100

    settings of the nativewind and tailwind

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -3,7 +3,7 @@ import { colors } from './src/styles/colors'
 import { fontFamily } from './src/styles/fontFamily'
 
 module.exports = {
-    content: ["./src.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+    content:["./src/**/*.{js,jsx,ts,tsx}"],
     presets: [require('nativewind/preset')],
     theme: {
     extend: {
