73dbae3685fd41967124b38e1404e1e8795917f0
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-07-09 16:37:41 +0100

    adds some validations to show different screen depending of the state application

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,12 +1,26 @@
 import { Input } from '@/components/input'
 import { View, Text, Image } from 'react-native'
+import { useState } from 'react'
 
 import { MapPin, Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight } from 'lucide-react-native'
 
 import { colors } from '@/styles/colors'
 import { Button } from '@/components/button'
 
+enum StepForm {
+    TRIP_DETAILS = 1,
+    ADD_EMAIL = 2
+}
+
 export default function Index() {
+    const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILS)
+
+    function handleNextStepForm(){
+        if(stepForm === StepForm.TRIP_DETAILS){
+            return setStepForm(StepForm.ADD_EMAIL)
+        }
+    }
+
     return(
         <View className='flex-1 items-center justify-center px-5'>
             <Image 
@@ -30,20 +44,28 @@ export default function Index() {
                     <Input.Field placeholder='When?'/>
                 </Input>
 
-                <View className='border-b py-3 border-zinc-800'>
-                    <Button variant='secondary'>
-                        <Button.Title>Change location/date</Button.Title>
-                        <Settings2 color={colors.zinc[200]} size={20}/>
-                    </Button>
-                </View>
+                { stepForm === StepForm.ADD_EMAIL && (
+                    <>
+                        <View className='border-b py-3 border-zinc-800'>
+                            <Button variant='secondary' onPress={() => setStepForm(StepForm.TRIP_DETAILS)}>
+                                <Button.Title>Change location/date</Button.Title>
+                                <Settings2 color={colors.zinc[200]} size={20}/>
+                            </Button>
+                        </View>
 
-                <Input>
-                    <UserRoundPlus color={colors.zinc[400]} size={20}/>
-                    <Input.Field placeholder='Who will be on the trip?'/>
-                </Input>
-
-                <Button>
-                    <Button.Title>Continue</Button.Title>
+                        <Input>
+                            <UserRoundPlus color={colors.zinc[400]} size={20}/>
+                            <Input.Field placeholder='Who will be on the trip?'/>
+                        </Input>
+                    </>
+                )}
+                
+                <Button onPress={handleNextStepForm}>
+                    <Button.Title>
+                        {
+                            stepForm === StepForm.TRIP_DETAILS ? 'Continue' : 'Confirm trip'
+                        }
+                    </Button.Title>
                     <ArrowRight color={colors.lime[950]} size={20}/>
                 </Button>
             </View>
